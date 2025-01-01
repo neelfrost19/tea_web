05bc67831fc66cae922b324040e61d9a551d985a
neelfrost19 <neelesh.das851@gmail.com>
2025-01-02 03:18:19 +0530

    -refactored css

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,19 +5,22 @@ import "../styles/Hero.css";
 
 const Hero = () => (
     <section className="hero">
-        <div className="hero-text">
-            <h1>
-                <span>Perfect Tea</span> Moments, Anytime, Anywhere.
-            </h1>
-            <p>
-                Discover the art of brewing with our premium tea crafted to deliver
-                flavor, aroma, and serenity in every sip.
-            </p>
-            <Button text="Explore Products" />
+        <div className="hero-container">
+            <div className="hero-text">
+                <h1>
+                    <span>Perfect Tea</span> Moments, Anytime, Anywhere.
+                </h1>
+                <p>
+                    Discover the art of brewing with our premium tea crafted to deliver
+                    flavor, aroma, and serenity in every sip.
+                </p>
+                <Button text="Explore Products"/>
+            </div>
+            <div className="hero-image">
+                <img src="/images/tea.svg" alt="Tea cup with leaves"/>
+            </div>
         </div>
-        <div className="hero-image">
-            <img src="/images/tea.svg" alt="Tea cup with leaves" />
-        </div>
+        <div>{"<--change here-->"}</div>
     </section>
 );
 
